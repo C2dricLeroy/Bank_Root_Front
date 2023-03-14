1f864474ad44c64b99190664b006428e33769c09
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-03-14 21:22:36 +0100

    add pages

---
+++ bank_root_front/src/app/pages/admin-space/admin-space.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminSpaceComponent } from './admin-space.component';
+
+
+
+@NgModule({
+  declarations: [
+    AdminSpaceComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminSpaceModule { }
