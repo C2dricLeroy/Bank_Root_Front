95a34d25398b108d904261c9f4c96637d379cb72
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-03-14 21:22:05 +0100

    add architecture

--- bank_root_front/src/app/app-routing.module.ts
+++ bank_root_front/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AdminSpaceComponent} from "./pages/admin-space/admin-space.component";
+import {HomepageComponent} from "./pages/homepage/homepage.component";
+import {PersonalSpaceComponent} from "./pages/personal-space/personal-space.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {RegistrationComponent} from "./pages/registration/registration.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin-space', component: AdminSpaceComponent },
+  { path: 'home', component: HomepageComponent },
+  { path: 'personal-space', component: PersonalSpaceComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bank_root_front/src/app/app.module.ts
+++ bank_root_front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { AccountListComponent } from './components/account-list/account-list.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    AccountListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
