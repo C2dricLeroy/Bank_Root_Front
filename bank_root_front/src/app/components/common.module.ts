e162d0bbc9349eba3082c06f6e81699ed7e2175e
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-03-15 12:13:17 +0100

    add a common module

---
+++ bank_root_front/src/app/components/common.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import {FooterComponent} from "./footer/footer.component";
+
+@NgModule({
+  declarations: [],
+  imports: [ FooterComponent
+  ],
+  exports : [
+  FooterComponent
+  ]
+})
+export class CommonComposentModule { }
