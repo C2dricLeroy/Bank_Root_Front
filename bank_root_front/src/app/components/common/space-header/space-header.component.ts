2575e817f060121ec902425b9df93463ee096d4b
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-03-16 15:45:17 +0100

    add common components

--- bank_root_front/src/app/components/common/space-header/space-header.component.ts
+++ bank_root_front/src/app/components/common/space-header/space-header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-space-header',
   templateUrl: './space-header.component.html',
   styleUrls: ['./space-header.component.css'],
+  imports: [RouterLink],
 })
 export class SpaceHeaderComponent {}
