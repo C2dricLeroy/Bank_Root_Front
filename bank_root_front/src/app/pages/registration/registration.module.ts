ce467cd9e88d02c78ead1c24a70a44b0d4087a94
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-03-17 17:10:39 +0100

    add pipes

--- bank_root_front/src/app/pages/registration/registration.module.ts
+++ bank_root_front/src/app/pages/registration/registration.module.ts
@@ -4,9 +4,16 @@ import { CommonComposentModule } from '../../components/common/common.module';
 import { RegistrationComponent } from './registration.component';
 import { RegisterFormComponent } from '../../components/register/register-form/register-form.component';
 import { RouterLink } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RegistrationComponent, RegisterFormComponent],
-  imports: [CommonModule, CommonComposentModule, RouterLink],
+  imports: [
+    CommonModule,
+    CommonComposentModule,
+    RouterLink,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class RegistrationModule {}
